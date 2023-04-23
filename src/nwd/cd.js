968552ade58da61835efdac52f263ee5ae30591a
Islambek Seytniyazov <islambek_seytniyazov@epam.com>
2023-04-23 15:49:59 +0500

    fix: fix undefined path bug

--- src/nwd/cd.js
+++ src/nwd/cd.js
@@ -2,6 +2,11 @@ import { sep, join } from "path";
 import { access } from "fs";
 
 async function cd(currentDir, path) {
+  if(path === undefined) {
+    return new Promise(resolve => {
+      resolve(currentDir);
+    })
+  }
   let pathArr = path.startsWith('.') ? path.slice(1).split('/') : path.split('/');
   pathArr = pathArr.filter(el => el.length !== 0);
   const correctPath = pathArr.join(sep);
